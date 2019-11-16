183e77cc49f82180f2226a6f81087df9503c9981
Steven Watkins <steven.watkins78@gmail.com>
2019-11-16 18:45:09 -0500

    handle greaterThan and lessThan

--- src/utils/filterData.js
+++ src/utils/filterData.js
@@ -1,6 +1,22 @@
-const handleEquals = (products, value) => {
+const handleEquals = (products, attr, value) => {
   return products.filter(pd =>
-    pd.property_values.find(pv => pv.value === value)
+    pd.property_values.find(
+      pv =>
+        (pv.value === value || pv.value === parseInt(value)) &&
+        pv.property_id === attr
+    )
+  );
+};
+
+const handleGreaterThan = (products, attr, value) => {
+  return products.filter(pd =>
+    pd.property_values.find(pv => pv.value > value && pv.property_id === attr)
+  );
+};
+
+const handleLessThan = (products, attr, value) => {
+  return products.filter(pd =>
+    pd.property_values.find(pv => pv.value < value && pv.property_id === attr)
   );
 };
 
@@ -9,7 +25,27 @@ const filterData = (products, filters) => {
 
   if (filters.queryString) {
     if (filters.selectedOperator === "equals") {
-      newProducts = handleEquals(products, filters.queryString);
+      newProducts = handleEquals(
+        products,
+        filters.selectedProperty.id,
+        filters.queryString
+      );
+    }
+
+    if (filters.selectedOperator === "greater_than") {
+      newProducts = handleGreaterThan(
+        products,
+        filters.selectedProperty.id,
+        filters.queryString
+      );
+    }
+
+    if (filters.selectedOperator === "less_than") {
+      newProducts = handleLessThan(
+        products,
+        filters.selectedProperty.id,
+        filters.queryString
+      );
     }
   }
 
